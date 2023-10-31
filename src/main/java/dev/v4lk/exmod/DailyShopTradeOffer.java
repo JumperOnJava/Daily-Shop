b5ad90e3696187b54bdab2e41885acee258a819e
ipz233_vvv <jumpergooog@gmail.com>
2023-10-31 08:26:06 +0200

    purchasable item icons

--- src/main/java/dev/v4lk/exmod/DailyShopTradeOffer.java
+++ src/main/java/dev/v4lk/exmod/DailyShopTradeOffer.java
@@ -1,9 +1,18 @@
 package dev.v4lk.exmod;
 
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.BundleTooltipComponent;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.item.TooltipData;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
-public class DailyShopTradeOffer{
+public class DailyShopTradeOffer implements TooltipData {
     private String color = Integer.toString(Formatting.DARK_GRAY.getColorIndex(),16).toUpperCase();
     private static final String DEFAULT_COLOR = "FF555555";
     public final Identifier toShopItem;
@@ -31,5 +40,60 @@ public class DailyShopTradeOffer{
     }
     public void setColor(int color) {
         this.color = Integer.toHexString(color);
+    }
+
+    public TooltipComponent getTooltipComponent(TooltipData tooltipData) {
+        return new TradeTooltipComponent(this);
+    }
+
+    private static class TradeTooltipComponent implements TooltipComponent {
+        private final DailyShopTradeOffer offer;
+
+        public TradeTooltipComponent(DailyShopTradeOffer dailyShopTradeOffer) {
+            this.offer = dailyShopTradeOffer;
+        }
+
+        @Override
+        public int getHeight() {
+            return 24;
+        }
+
+        @Override
+        public int getWidth(TextRenderer textRenderer) {
+            var purchaseText = textRenderer.getWidth(Text.translatable("exmod.tooltip.purchase"));
+            //var fromShopItem = null;
+            var forText = textRenderer.getWidth(Text.translatable("exmod.tooltip.for"));
+            //var toShopItem = null;
+            return purchaseText + 18*2 + 2*2 +forText;
+        }
+
+        @Override
+        public void drawItems(TextRenderer textRenderer, int x, int y, DrawContext context) {
+            context.getMatrices().push();
+            context.getMatrices().translate(x,y,0);
+            var purchasableText = Text.translatable("exmod.avaliable").setStyle(Style.EMPTY.withColor(offer.getColor()));
+            context.drawText(textRenderer,purchasableText,0,0,0xFFFFFFFF,false);
+
+            int position = 0;
+            var purchaseText = Text.translatable("exmod.tooltip.purchase");
+            var forText = Text.translatable("exmod.tooltip.for");
+            var fromShopItem = new ItemStack(Registries.ITEM.get(offer.fromShopItem), offer.fromShopAmount);
+            var toShopItem = new ItemStack(Registries.ITEM.get(offer.toShopItem), offer.toShopAmount);
+            context.getMatrices().translate(0,8,0);
+            context.drawText(textRenderer,purchaseText,position,4,0xFFFFFFFF,false);
+            position+=2;
+            position+=textRenderer.getWidth(purchaseText);
+            context.drawItem(toShopItem,position,0);
+            context.drawItemInSlot(textRenderer,toShopItem,position,0);
+            position+=18;
+            context.drawText(textRenderer,forText,position,4,0xFFFFFFFF,false);
+            position+=textRenderer.getWidth(forText);
+            position+=2;
+            context.drawItem(fromShopItem,position,0);
+            context.drawItemInSlot(textRenderer,fromShopItem,position,0);
+            position+=18;
+            context.getMatrices().pop();
+
+        }
     }
 }
--- src/main/java/dev/v4lk/exmod/client/ExchangemachinemodClient.java
+++ src/main/java/dev/v4lk/exmod/client/ExchangemachinemodClient.java
@@ -5,6 +5,9 @@ import dev.v4lk.exmod.DailyShopTradeOffer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.item.TooltipData;
 import net.minecraft.registry.Registries;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -17,7 +20,8 @@ import java.util.Map;
 
 public class ExchangemachinemodClient implements ClientModInitializer {
     public static List<DailyShopTradeOffer> trades = new ArrayList<>();
-    private static Map<Identifier,DailyShopTradeOffer> getTradeMap(){
+
+    public static Map<Identifier,DailyShopTradeOffer> getTradeMap(){
         var tradeMap = new HashMap<Identifier, DailyShopTradeOffer>();
         trades.forEach(t->tradeMap.put(t.toShopItem,t));
         return tradeMap;
@@ -25,12 +29,21 @@ public class ExchangemachinemodClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientPlayConnectionEvents.INIT.register(ConfigSynchronizer::client);
+        TooltipComponentCallback.EVENT.register(this::tooltipContextEvent);
         ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
             var tmap = getTradeMap();
+            //stack.getTooltip()
             var dailyShopTradeOffer = tmap.get(Registries.ITEM.getId(stack.getItem()));
             if(dailyShopTradeOffer==null)
                 return;
-            lines.add(Text.translatable("exmod.avaliable").setStyle(Style.EMPTY.withColor(dailyShopTradeOffer.getColor())));
+            //lines.add();
         });
+    }
+
+    private TooltipComponent tooltipContextEvent(TooltipData tooltipData) {
+        if(tooltipData instanceof DailyShopTradeOffer offer){
+            return offer.getTooltipComponent(tooltipData);
+        }
+        return null;
     }
 }
